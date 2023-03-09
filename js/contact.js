0a125f32a09335cd60b9ddda27dcfdbe962e636b
RomanMillan <romandos962m@gmail.com>
2023-03-09 12:25:37 +0100

    casi validación completa

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,26 @@
-const form = document.getElementById("form");
+// const form = document.getElementById("form");
 /* const nameUser = document.getElementById("nameUser");
  */
-const inputs = document.querySelectorAll('#form input')
+// const inputs = document.querySelectorAll('#form input')
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
